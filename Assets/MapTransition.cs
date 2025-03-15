5b74c44cdeed699fd4568c0b6258abc9174a2591
Maxi Dominguez <maxidominguez033@gmail.com>
2025-03-15 11:12:18 -0300

    Added Camera Movement
    Now the camera follows the player
    
    -Implented a cinemachine for camera manipulation
    
    - Now you can explore outside the village with the new camera transition !

---
+++ Assets/MapTransition.cs
@@ -0,0 +1,53 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] private PolygonCollider2D mapBoundry;
+    private CinemachineConfiner2D confiner;
+    [SerializeField] private Direction direction;
+    [SerializeField] private float additivePos = 2f;
+
+    //Call for the direction that we want to teleport
+    private enum Direction { Up, Down, Left, Right }
+
+    private void Awake()
+    {
+
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundry;
+            confiner.InvalidateBoundingShapeCache();
+
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector2 newPos = player.transform.position; 
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x -= additivePos; 
+                break;
+            case Direction.Right:
+                newPos.x += additivePos; 
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+}
